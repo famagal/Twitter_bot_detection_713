f62b575a619c36369e50b6a323da7f8e7ef0213c
Daniel Marinescu <danielmarinescu2@gmail.com>
2021-11-22 10:37:30 -0500

    Added new code and jobs for environmental variables'

--- Twitter_bot_detection_713/__init__.py
+++ Twitter_bot_detection_713/__init__.py
@@ -1,5 +1,11 @@
 from os.path import isfile
 from os.path import dirname
+from os.path import join
+from dotenv import load_dotenv
+
+env_path = join(dirname(dirname(__file__)), '.env')  # ../.env
+load_dotenv(dotenv_path=env_path)
+# Whenever needed, can be called via os.getenv('API_KEY')
 
 version_file = '{}/version.txt'.format(dirname(__file__))
 
